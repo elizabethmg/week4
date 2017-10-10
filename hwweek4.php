e053706e9b228f18c291f27b61940686dc6dfc50
Elizabeth Gaspar <eg222@afsaccess1.njit.edu>
2017-10-10 14:31:02 -0400

    Added file

---
+++ hwweek4.php
@@ -0,0 +1,57 @@
+<?php
+
+$date =  date('Y/m/d', time());
+echo "The value of \$date: ".$date."<br>";
+echo "<br>";
+
+$tar = "2017/05/24";
+echo "The value of \$tar: ".$tar."<br>";
+echo "<br>";
+
+if(i>0){
+	echo"the future";
+} elseif(i<0){
+	echo"the past";
+} else{
+	echo"Oops";
+}
+echo"<br>";
+
+echo"<br>";
+
+echo strpos($tar, "/")."<br>";
+echo strrpos($tar, "/");
+echo"<br>";
+
+echo"<br>";
+
+echo stripos($date)."<br>";
+echo"<br>";
+
+echo str_word_count($tar)."<br>";
+echo"<br>";
+
+echo strlen($tar)."<br>";
+echo"<br>";
+
+echo ord($tar)."<br>";
+echo"<br>";
+
+echo substr($date,-2)."<br>";
+echo"<br>";
+
+
+
+
+$year = array("2012", "396", "300","2000", "1100", "1089");
+echo "The value of \$year: ";
+echo "<br>";
+print_r($year)
+
+//foreach ($year as $value) {
+//    $value = ;
+//}
+
+
+
+?>
